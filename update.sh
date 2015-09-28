5e90f4de513c95dc6706b85a7eb94960045c22a4
Han Xiao <han.xiao@zalando.de>
2015-09-28 18:21:23 +0200

    regular update of database Mon Sep 28 18:21:23 CEST 2015

--- update.sh
+++ update.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 HOME=/home/han
 set -e
-AGENT="ssh-agent -s"
-if [ ! -d $HOME/.ssh/agent ]; then
-        mkdir -p $HOME/.ssh/agent
-fi
-#
-# Start an agent if there isn't one running already.
-#
-pid=`ps -u$LOGNAME | grep ssh-age | awk '{print $1}'`
-if [ -z "$pid" ]; then
-        $AGENT | grep -v echo > $HOME/.ssh/agent/$HOST & pid=$!
-        sleep 1 # Let it fork and stuff
-fi
-ssh-add $HOME/.ssh/id_rsa
-ssh-add -l
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 source $HOME/.keychain/${HOSTNAME}-sh
 /usr/bin/java -jar /home/han/Documents/xiaode-backend/target/xiaode.jar --json /home/han/Documents/xiaode-backend/database.json --kw /home/han/Documents/xiaode-backend/keywords.json
 /home/han/Documents/xiaode-backend/nohup.out
